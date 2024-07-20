22aededc8f10dce9e43fe89c19f24348e1ba7300
Hassan Hameed <hh2910097@gmail.com>
2024-07-20 14:37:52 +0500

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ function App() {
     <>
     <div className="bg-gradient-to-b from-indigo-950 via-blue-800 to-sky-600 min-h-screen w-full fixed">
       <h1 className="flex justify-center p-12 text-6xl font-bold font-serif text-white">Count The Value</h1>
-      <div className="font-semibold text-[350px] text-white flex justify-center items-center">{counterState.count}</div>
+      <div className="font-semibold text-[200px] text-white flex justify-center items-center">{counterState.count}</div>
       <Counter />
       </div>
     </>
--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -8,8 +8,8 @@ const Counter = () => {
   return (
     <>
     <div className=' text-5xl px-12 text-blue-50 font-mono font-extrabold' >
-    <button className='absolute left-10 top-[410px]' onClick={ () => contextUpdate.setCount(contextUpdate.count + 1)}>Increment</button>
-    <button  className='absolute right-10 top-[410px]' onClick={ () => contextUpdate.setCount(contextUpdate.count - 1)}>Decrement</button>
+    <button className='absolute left-10 top-[310px]' onClick={ () => contextUpdate.setCount(contextUpdate.count + 1)}>Increment</button>
+    <button  className='absolute right-10 top-[310px]' onClick={ () => contextUpdate.setCount(contextUpdate.count - 1)}>Decrement</button>
     </div>
     
     </>
